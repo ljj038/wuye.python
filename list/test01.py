041e7ef78e00acab522eb04f25c197b40ee87d74
赵英楠 <791520450@qq.com>
2016-08-02 20:39:04 +0800

    python list functions

--- list/test01.py
+++ list/test01.py
@@ -38,3 +38,81 @@ print squares;#[]
 squares.append(101);
 squares.append(102);
 print len(squares);#2
+
+#删除元素
+list1 = ['a', 'b', 'c', 'd', 'e', 'f'];
+print list1;#['a', 'b', 'c', 'd', 'e', 'f']
+del list1[0];
+print list1;#['b', 'c', 'd', 'e', 'f']
+
+#列表重复
+list2 = ['a'];
+print list2 * 4;#['a', 'a', 'a', 'a']
+
+#迭代
+for w in list1:
+    print w;
+
+for index in range(len(list1)):
+    print list1[index];
+
+#list1.append('a');
+#判断元素是否存在于列表中
+if 'a' in list1:
+    print 'a in list1';
+else:
+    print 'a not in list1';
+
+
+#获取列表的最大/最小值
+list3 = [1,2,3,4];
+print max(list3);#4
+print min(list3);#1
+list4 = [6,7,8,9];
+print max(list4);#9
+print min(list4);#6
+
+
+#在列表末尾追加
+list3.append(10);
+print list3;#[1, 2, 3, 4, 10]
+list3.append([10,11,12]);
+print list3;#[1, 2, 3, 4, 10, [10, 11, 12]]
+
+#在列表的末尾一次性追加另一个列表中的多个值
+list5 = ['a', 'b', 'c', 'd'];
+list6 = [1];
+list6.extend(list5);
+print list6;#[1, 'a', 'b', 'c', 'd']
+
+#统计每个元素在列表中出现的次数
+i = 1;
+while i<=3:
+    list3.append(10);
+    i += 1;
+
+print list3;#[1, 2, 3, 4, 10, [10, 11, 12], 10, 10, 10]
+print list3.count(10);#4
+
+#从列表中找出某个值第一个匹配项的索引值
+list7 = [1,2,3,1,2,3];
+print list7.index(2);#1
+
+#将对象插入列表,只是插入到指定的位置，索引会依次顺延
+list7.insert(3,100);
+print list7;#[1, 2, 3, 100, 1, 2, 3]
+
+#移除列表中的某个值的第一个匹配项
+list7.remove(1);
+print list7;#[2, 3, 100, 1, 2, 3]
+
+#移除列表中的一个元素（默认最后一个元素），并且返回该元素的值
+print list7.pop(0);#2
+
+#反转列表的元素
+list7.reverse();
+print list7;#[3, 2, 1, 100, 3]
+
+#排序
+list7.sort();
+print list7;#[1, 2, 3, 3, 100]
