4b6a5214f599299e102b4ef74815b4e1cef1d08a
赵英楠 <791520450@qq.com>
2016-08-24 12:14:52 +0800

    test PyCharm

---
+++ PyCharm/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# coding=utf-8
+
+
+class Test:
+    def __init__(self):
+        pass
+    def execute(self):
+        print 1
+if(__name__ == '__main__'):
+    test = Test()
+    test.execute()
